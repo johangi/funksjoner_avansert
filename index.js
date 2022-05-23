975ec3e76c319ddf77a98661b8b136ef42814cdf
johangi <johangiaever@gmail.com>
2022-05-23 14:19:54 +0200

    fixing minor issues

--- index.js
+++ index.js
@@ -2,40 +2,43 @@ function add(...numbers){
     const sum = numbers.reduce((accumulator, value) => {
         return accumulator + value;
     }, 0);
-    console.log(sum);
+    return sum
 }
-
-add(1, 7, 8);
+let addResult = add(2, 6, 8);
+console.log(addResult);
 
 function subtract(...numbers){
     const sum = numbers.reduce((accumulator, number) => {
         return accumulator - number;
     });
-    console.log(sum);
+    return sum
 }
-
-subtract(10, 1, 2);
+let subtractResult = subtract(10, 1, 2);
+console.log(subtractResult);
 
 function divide(...numbers){
     const sum = numbers.reduce((accumulator, number) => {
         return accumulator / number;
     });
-    console.log(sum);
+    return sum
 }
-
-divide(420, 2);
+let divideResult = divide(420, 2);
+console.log(divideResult);
 
 function multiply(...numbers){
     const sum = numbers.reduce((accumulator, number) => {
         return accumulator * number;
     });
-    console.log(sum);
+    return sum
 }
-
-multiply(2, 27);
+let multiplyResult = multiply(2, 27);
+console.log(multiplyResult);
 
 function logResult(mathFunction, ...number){
     console.log(mathFunction(...number));
 }
 
-logResult(add, 2, 7)
+logResult(add, 2, 7);
+logResult(subtract, 7, 2);
+logResult(divide, 14, 7);
+logResult(multiply, 7, 7);
